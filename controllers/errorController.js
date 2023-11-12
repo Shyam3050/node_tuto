b11b5d866ef0bd92153da4f779dd8d060d37952c
Shyam3050 <nimmanashyam3050@gmail.com>
2023-11-12 22:04:15 +0530

    error during dev and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,33 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.static || "error";
+function sendErrDev(err, res) {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    error: err,
+    stack: err.stack
   });
-  next()
+}
+function sendErrPrd(err, res) {
+  if(err.isOperational){
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }else{
+    console.log("ERROR",err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Sometime went wrong'
+    })
+  }
+}
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.static || "error";
+  if (process.env.NODE_ENV === "development") {
+    sendErrDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrPrd(err, res);
+  }
+ 
+  next();
 };
