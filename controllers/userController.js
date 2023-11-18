f5c71516165f64080aaab1821a5258ef7c821176
Shyam3050 <nimmanashyam3050@gmail.com>
2023-11-18 09:45:42 +0530

    updating current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,44 @@
+const User = require("../models/userModal");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
 
+const filteredObj = (body, ...allowFields) => {
+  const newObj = {};
+  Object.keys(body).forEach((field) => {
+    if (allowFields.includes(field)) newObj[field] = body[field];
+  });
+  return newObj;
+};
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: "success",
+    result: users.length,
+    data: users,
+  });
+});
+
+exports.update = catchAsync( async(req, res, next) => {
+  // if users post password then push error
+  if (req.body.password || req.body.confirmPassword) {
+    return next(
+      new AppError(
+        "this route not for update password for update checkout /updatepassword",
+        400
+      )
+    );
+  }
+  // update password
+  const filteredField = filteredObj(req.body, "name", "email");
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredField, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: updatedUser
+  });
+})
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authController = require("./../controllers/authController");
+const userController = require("../controllers/userController")
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.route("/login").post(authController.login);
 router.route("/forgotpassword").post(authController.forgotPassword);
 router.route("/resetpassword/:token").patch(authController.resetPassword);
 router.route("/updatepassword").patch(authController.protect, authController.updatepassword)
+router.route("/updateme").patch(authController.protect, userController.update)
 
 module.exports = router;
