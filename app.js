e09d93539b428913799383696b1bb847176f4169
Shyam3050 <nimmanashyam3050@gmail.com>
2023-11-19 10:36:08 +0530

    rate limit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const globlErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 const viewRouter = require("./routes/viewRouter");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
@@ -13,6 +14,14 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+//rate limit
+const limiter = rateLimit({
+  max: 5,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP, wait for an hour",
+});
+app.use("/api", limiter);
 
 app.use(express.json());
 
@@ -30,7 +39,7 @@ app.use("/", viewRouter);
 
 // handlong unhandled routes
 app.all("*", (req, res, next) => {
-  next(new AppError(`this path ${req.originalUrl} is not found.`,400));
+  next(new AppError(`this path ${req.originalUrl} is not found.`, 400));
 });
 
 app.use(globlErrorHandler);
